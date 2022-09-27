1a603cf8a0e81cfa24cac5cd797a87c8b7953fd4
Kamal Mukkamala <kamalnrf@gmail.com>
2022-09-28 01:09:11 +0530

    Add development enviroment variables

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv'
+if (process.env.ENVIROMENT === 'development') {
+  dotenv.config()
+}
+
 import logger, {LogLevelDesc} from 'loglevel'
 import startServer from './start'
 
 const logLevel = (process.env.LOG_LEVEL ?? 'info') as LogLevelDesc
-
 logger.setLevel(logLevel)
 
 startServer({port: 8080})
