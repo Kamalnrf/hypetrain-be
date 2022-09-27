92bd95083f1cef212f78a5a850a6e131b75d8836
Kamal Mukkamala <kamalnrf@gmail.com>
2022-09-28 01:10:00 +0530

    Add api routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,20 @@
 import express from 'express'
+import {register, registerEmail} from '../controllers/authController'
+import {me} from '../controllers/userController'
+import {authMiddleware, verifyUserId} from '../utils/auth'
+import {
+  getPreferences,
+  updatePreferences,
+} from '../controllers/preferencesController'
 
 function getRouter() {
   const router = express.Router()
-  router.get('/', (_, res) => {
-    res.json({
-      success: true,
-    })
-  })
+  router.post('/register', register)
+  router.post('/register-email', authMiddleware, verifyUserId, registerEmail)
+  router.get('/me', authMiddleware, verifyUserId, me)
+  router.get('/preference', authMiddleware, verifyUserId, getPreferences)
+  router.put('/preference', authMiddleware, verifyUserId, updatePreferences)
+
   return router
 }
 export default getRouter
