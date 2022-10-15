02a451be1cbe50178659035fbf0de576833d1265
Kamal Mukkamala <kamalnrf@gmail.com>
2022-10-15 23:23:18 +0530

    Add updated migration files

--- prisma/migrations/20221010190955_add_activity_and_queue_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `isHyped` to the `TweetQueue` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "TweetQueue" ADD COLUMN     "isHyped" BOOLEAN NOT NULL;
